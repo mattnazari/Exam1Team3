ce6aa67b09722dad94145f001cee5d3dace6ed56
matthewnazari <matthew.nazari8@gmail.com>
2019-10-16 09:23:17 -0700

    Finished Story

---
+++ __tests__/Story-test.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+import Story from '../comps/Story';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+  renderer.create(<Story />);
+});
