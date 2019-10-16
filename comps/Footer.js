808075d38e08dd21df568924b213560c1c419f50
tashavithaya <tashavithaya@gmail.com>
2019-10-16 09:52:25 -0700

    fixed footer, styles

--- comps/Footer.js
+++ comps/Footer.js
@@ -2,45 +2,28 @@ import React, {useState} from 'react';
 import {View, Text, Picker} from 'react-native';
 import styles from '../styles/FooterStyles';
 
+
 const Footer = ()=>{
 
-    const [name, setName] = useState(1)
-
-    var Author = null;
-    var footer = styles.footer
-
-    if(name == 1){
-        Author = (
-            <Text>Sam Marles</Text>
-        )
-    }
-    if(name == 2){
-        Author = (
-            <Text>Jesse Jen</Text>
-        )
-    }
-    if(name == 3){
-        Author = (
-            <Text>Chaze Style</Text>
-        )
-    }
+    const [name, setName] = useState('author full name')
 
     return(
-        <View style={footer}>
+        <View style={[styles.container]}>
+            <Text style={styles.byText}>by</Text>
+            <Text style={styles.titleText}>{name}</Text>
             <Picker style={styles.picker}
             onValueChange={(value)=>{setName(value)}}
             selectedValue={name}
             >
-            <Text>by {Author}</Text>
-            <Picker.Item label='Story 1' value='1'/>
-            <Picker.Item label='Story 2' value='2'/>
-            <Picker.Item label='Story 3' value='3'/>
+
+            <Picker.Item label='Story 1' value=' Sam Marles'/>
+            <Picker.Item label='Story 2' value=' Jesse Jen'/>
+            <Picker.Item label='Story 3' value=' Chaze Style'/>
             </Picker>
         
         </View>
     )
 };
-
 
 
 export default Footer;
--- styles/FooterStyles.js
+++ styles/FooterStyles.js
@@ -1,17 +1,34 @@
 import {StyleSheet} from 'react-native';
 
-var styles = StyleSheet.create({
-    footer:{
+const styles = StyleSheet.create({
+    container:{
         flex:1,
         justifyContent:"center",
         alignItems:"center",
+        flexDirection:"row",
         backgroundColor:"#ffcc99",
+        padding:20,
+
     },
-
     picker:{
-        width:100
-    }
-});
+        width:150,
+        
+    },
+    titleText: {
+        flexDirection:"row",
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontStyle:'italic',
+        paddingTop:15,
+        paddingRight:15,
+      },
 
+    byText: {
+        fontSize: 10,
+        fontWeight: '100',
+      },
+    
+
+})
 
 export default styles; 
