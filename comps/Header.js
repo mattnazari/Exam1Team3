ee6bf80a88e7cb2190f103b64b2f1b9af3c825b8
Ashlynn Chen <a01061395@se14l0120-05.ad.bcit.ca>
2019-10-16 11:16:00 -0700

    commiting

--- comps/Header.js
+++ comps/Header.js
@@ -3,38 +3,33 @@ import {View, Text, Button, TextInput} from 'react-native';
 import styles from '../styles/HeaderStyles';
 
 function Header() {
-const [titleTxt, SetTitleTxt] = useState('The Story of My Life');
-const [inputStatus, SetInputStatus] = useState(false);
+  const [titleTxt, SetTitleTxt] = useState('The Story of My Life');
+  const [inputStatus, SetInputStatus] = useState(false);
 
-var showInputText = null;
-if (inputStatus == true) {
-showInputText = (
-<TextInput
-onChangeText={txt => {
-SetTitleTxt(txt);
-}}
-value={titleTxt}
-placeholder="The Story of My Life"
-/>
-);
-} else {
-showInputText = null;
-}
-
-return (
-
-
-{titleTxt}
-<Button
-  title='Edit'
-  onPress={() => {
-  SetInputStatus(!inputStatus);
-  }}
-/>
-
-{showInputText}
-
-);
+  var showInputText = null;
+  if (inputStatus == true) {
+    showInputText = (
+      <TextInput
+      onChangeText={txt => {
+        SetTitleTxt(txt);
+      }}
+      value={titleTxt}
+      placeholder="The Story of My Life"
+      />
+    );
+  } else {
+    howInputText = null;
+  }
+  return (
+    {titleTxt}
+    <Button
+    title='Edit'
+    onPress={() => {
+      SetInputStatus(!inputStatus);
+    }}
+    />
+    {showInputText}
+  );
 }
 
 export default Header;
