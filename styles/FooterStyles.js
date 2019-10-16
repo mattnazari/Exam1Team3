91c231655a2347b86fe597a8efe457f3e4f6b299
tashavithaya <tashavithaya@gmail.com>
2019-10-16 09:09:22 -0700

    added footer

---
+++ styles/FooterStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+    footer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#ffcc99",
+    },
+
+    picker:{
+        width:100
+    }
+});
+
+export default styles; 
